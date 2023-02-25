923da015d8cd2bdf1bbd83032081c65867fc9982
Chen <2810582161@qq.com>
2023-02-25 21:59:33 +0800

    初步使用tree方式，效果不理想，所以改用文件夹方式

---
+++ ruoyi-ui/src/views/store/excel/profitStatement.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="custom-tree-container">
+    <div class="block">
+      <p>使用 scoped slot</p>
+      <el-tree :data="data" show-checkbox node-key="id" default-expand-all :expand-on-click-node="false">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+          <span>{{ node.label }}</span>
+          <span>
+            <span>预览</span>
+            <span>删除</span>
+          </span>
+        </span>
+      </el-tree>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "profitStatement",
+  data () {
+    const data = [{
+      id: 1,
+      label: '一级 1',
+      children: [{
+        id: 4,
+        label: '二级 1-1'
+      }]
+    }];
+    return {
+      data: JSON.parse(JSON.stringify(data)),
+      data: JSON.parse(JSON.stringify(data))
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  padding-right: 8px;
+}
+</style>
--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -13,7 +13,7 @@ const port = process.env.port || process.env.npm_config_port || 80 // 端口
 
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
-// 这里只列一部分，具体配置参考文档
+// 这里只列一部分，具体配置参考文档prod-api
 module.exports = {
   // 部署生产环境和开发环境下的URL。
   // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
@@ -35,7 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        target: `http://localhost:9988`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
